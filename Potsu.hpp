ca14567d2aee6fc44f1f2e99a4e33855bf3da802
Oli Hills <GrumpyTurtl3@proton.me>
2024-06-18 08:06:40 +1200

    hhaahha

--- Potsu.hpp
+++ Potsu.hpp
@@ -1,85 +1,127 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 struct Vec2{
-    int x;
-    int y;
+    float x;
+    float y;
 };
 
 struct Vec3{
-    int x;
-    int y;
-    int z;
+    float x;
+    float y;
+    float z;
 };
 
-// struct PolygonCollider{
-
-// }
-
-struct SquareCollider{
-    Vec2 offset = {0,0};
-    int width;
-    int Height;
+struct AABB{
+    Vec2 min;
+    Vec2 max;
 };
 
-struct CircleCollider{
-    int radius;
+struct Circle{
+    float radius;
+    Vec2 position;
 };
 
-class StaticBody{
-
-};
-
-class RigidBody{
-public:
-
-        void CreateSquareCollider(){
-            SquareCollider Collider;
-        }
-
-        void CreateCircleCollider(){
-            CircleCollider Collider;
-        }
-
-        int mass;
-        Vec2 position;
-        Vec3 velocity;
-        Vec3 acceleration;
-
-private:
-
-};
-
-struct Physics{
-public: 
-    double DotProduct(){
-
+struct Object{
+    Vec2 velocity;
+    float restitution;
+    float mass = 1;
+    if(mass == 0){
+        float invMass = 0;
+    }else{
+        float invMass = 1 / mass;
     }
+    
+}
 
-    void ResolveCollision(RigidBody A, RigidBody B){
-        Vec2 rv = B.velocity - A.velocity;
+struct Manifold{
+    Object *A;
+    Object *B;
+    float penetration;
+    Vec2 normal;
+};
 
-        float velAlongNormal = 
-    }
+float DotProduct(Vec2 A, Vec2 B){
+
 }
 
 
-class World{
-public:
-    std::vector<RigidBody*> RigidBodyObjects;
-    std::vector<StaticBody*> StaticObjects;
+bool AABBvsAABB(AABB a, AABB b){
+    if(a.max.x < b.min.x || a.min.x > b.max.x) return false;
+    if(a.max.y < b.min.y || a.min.y > b.max.y) return false;
 
-    RigidBody* CreateRigidBody() {
-        RigidBody* newRigidBody = new RigidBody;  // Allocate memory dynamically
-        RigidBodyObjects.push_back(newRigidBody);  // Add pointer to the container
-        return newRigidBody;                         // Return the pointer
+    return true;
+}
+
+bool CircleVsCircle(Manifold *m){
+    Object *A = m->A;
+    Object *B = m->B;
+
+    Vec2 n = B->position - A->position;
+
+    float r = a.radius + b.radius;
+    r *= r;
+    
+    if(n^2 > r){
+        return false;
     }
 
-    StaticBody* CreateStaticBody() {
-        StaticBody* newStaticBody = new StaticBody;
-        StaticObjects.push_back(newStaticBody);
-        return newStaticBody;
+    if(n != 0){
+        
+        m->penetration = r - n;
+
+        m->normal =  r / n;
+        return true;
+    }else{
+        m->penetration = A->radius;
+        m->normal = Vec2(1,0);
+        return true;
     }
-};
+
+}
+
+
+void PositionalCorrection( Object A, Object B )
+
+{
+
+  const float percent = 0.2 // usually 20% to 80%
+
+  const float slop = 0.01 // usually 0.01 to 0.1
+
+  Vec2 correction = max( penetration - slop, 0.0f ) / (A.inv_mass + B.inv_mass)) * percent * n
+
+  A.position -= A.inv_mass * correction
+
+  B.position += B.inv_mass * correction
+
+}
+
+
+void ResolveCollision(Object A, Object B, Vec2 normal){
+    Vec2 rv = B.velocity - A.velocity;
+
+    float velAlongNormal = DotProduct(rv, normal);
+
+    if(velAlongNormal > 0){
+        return;
+    }
+    
+    float e = min(A.restitution, B.restitution);
+
+    float j = -(1 + e) * velAlongNormal;
+    j /= A.invMass + B.invMass;
+
+    Vec2 impulse = j * normal;
+    float mass_sum = A.mass + B.mass
+
+    float ratio = A.mass / mass_sum
+    A.velocity -= ratio * impulse
+
+    ratio = B.mass / mass_sum
+    B.velocity += ratio * impulse
+
+}
 
--- main.cpp
+++ main.cpp
@@ -2,10 +2,4 @@
 #include <iostream>
 
 int main(){
-    World World;
-    RigidBody* rb = World.CreateRigidBody();
-    rb->CreateSquareCollider();
-    rb->mass = 10;
-    
-    std::cout << rb->mass << "\n" << rb->position.x << "\n" << rb->position.y << std::endl;
 }
